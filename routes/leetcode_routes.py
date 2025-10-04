00d223932b7360d55cac4e83b3b46fc4e2a4fde6
Priyankar_010_ <109033813+PriyankarCode010@users.noreply.github.com>
2025-10-04 20:02:45 +0530

    final

--- routes/leetcode_routes.py
+++ routes/leetcode_routes.py
@@ -4,5 +4,5 @@ from utils.leetcode_scraper import scrape_leetcode_stats
 router = APIRouter(prefix="/leetcode", tags=["LeetCode"])
 
 @router.get("/{username}")
-def get_leetcode_data(username: str):
-    return scrape_leetcode_stats(username)
+def get_leetcode_data(username: str, url: str = None):
+    return scrape_leetcode_stats(username,url)
