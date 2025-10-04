4ba4b20ca488e4d59bef1fe5d44a63e944e8f9b4
Priyankar_010_ <109033813+PriyankarCode010@users.noreply.github.com>
2025-10-04 19:15:06 +0530

    final

--- main.py
+++ main.py
@@ -1,25 +1,13 @@
-import os
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from routes import leetcode_routes, gfg_routes, github_routes
+from routes import gfg_routes, github_routes, leetcode_routes
 
 app = FastAPI()
 
-# Routers
-app.include_router(leetcode_routes.router, prefix="/leetcode")
-app.include_router(gfg_routes.router, prefix="/gfg")
-app.include_router(github_routes.router, prefix="/github")
+# Register routes
+app.include_router(gfg_routes.router)
+app.include_router(github_routes.router)
+app.include_router(leetcode_routes.router)
 
-# CORS
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-# Run uvicorn only if local
-if __name__ == "__main__":
-    # port = int(os.environ.get("PORT", 8080))
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+@app.get("/")
+def root():
+    return {"message": "Mystara Backend is running 🚀"}
