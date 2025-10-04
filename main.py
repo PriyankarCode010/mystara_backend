2492ed5e62a2e1bb289392a1d3f33519599e2031
Priyankar_010_ <109033813+PriyankarCode010@users.noreply.github.com>
2025-10-04 19:04:44 +0530

    final

--- main.py
+++ main.py
@@ -20,6 +20,6 @@ app.add_middleware(
 
 # Run uvicorn only if local
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 1004))
+    port = int(os.environ.get("PORT", 8080))
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=port)
